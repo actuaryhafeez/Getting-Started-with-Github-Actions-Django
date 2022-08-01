c580c907d7fb1806d09721bf2a9cb0e9d031f212
Abdul Hafeez <ahdatascientist@gmail.com>
2022-08-01 08:20:56 +0500

    update code

--- ah_home/django_github_action/settings.py
+++ ah_home/django_github_action/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e@r*bmee@j5=!%7y)&65pvi6a+^eha2p9)7b=^g-2f6jgyk(3o'
+SECRET_KEY = 'django-insecure-)0wm-7ukv0l$#@)_$ic*w=$(=wth-&-ul)eeks!xokog)8mx=7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
